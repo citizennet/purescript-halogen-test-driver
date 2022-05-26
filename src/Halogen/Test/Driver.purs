e2ce4b4049d303e66ac9bea669e67810e35d25ad
kai.dao <kai.dao@platonic.systems>
2022-05-26 08:41:48 +0000

    Tidy with `unicode: never`
    It seems the team was pretty clear about wanting these symbols gone

--- src/Halogen/Test/Driver.purs
+++ src/Halogen/Test/Driver.purs
@@ -121,11 +121,11 @@ buildVDom renderChildRef = build
   build :: VDomMachine action slots
   build =
     Effect.Uncurried.mkEffectFn1 case _ of
-      Halogen.VDom.Text _ → Effect.Uncurried.runEffectFn1 buildText build
-      Halogen.VDom.Elem _ _ _ children → Effect.Uncurried.runEffectFn1 buildElem { build, children }
-      Halogen.VDom.Keyed _ _ _ children → Effect.Uncurried.runEffectFn1 buildKeyed { build, children }
-      Halogen.VDom.Widget slot → Effect.Uncurried.runEffectFn1 buildWidget { build, renderChildRef, slot }
-      Halogen.VDom.Grafted g → do
+      Halogen.VDom.Text _ -> Effect.Uncurried.runEffectFn1 buildText build
+      Halogen.VDom.Elem _ _ _ children -> Effect.Uncurried.runEffectFn1 buildElem { build, children }
+      Halogen.VDom.Keyed _ _ _ children -> Effect.Uncurried.runEffectFn1 buildKeyed { build, children }
+      Halogen.VDom.Widget slot -> Effect.Uncurried.runEffectFn1 buildWidget { build, renderChildRef, slot }
+      Halogen.VDom.Grafted g -> do
         Effect.Uncurried.runEffectFn1 build (Halogen.VDom.runGraft g)
 
 -- | Adapted from `Halogen.VDom.DOM.TextState`
@@ -133,46 +133,46 @@ buildVDom renderChildRef = build
 -- | * removed `value :: String`
 -- |   * we don't need to compare and patch in DOM
 type TextState a w =
-  { build ∷ VDomMachine a w
+  { build :: VDomMachine a w
   }
 
 -- | Adapted from `Halogen.VDom.DOM.buildText`
-buildText ∷
+buildText ::
   forall a w.
   Effect.Uncurried.EffectFn1
     (VDomMachine a w)
     (VDomStep a w)
 buildText =
-  Effect.Uncurried.mkEffectFn1 \build → do
+  Effect.Uncurried.mkEffectFn1 \build -> do
     let
       state :: TextState a w
       state = { build }
     pure $ Halogen.VDom.mkStep $ Halogen.VDom.Step unit state patchText haltText
 
 -- | Adapted from `Halogen.VDom.DOM.patchText`
-patchText ∷
+patchText ::
   forall a w.
   Effect.Uncurried.EffectFn2
     (TextState a w)
     (VHTML a w)
     (VDomStep a w)
 patchText =
-  Effect.Uncurried.mkEffectFn2 \state vdom → do
+  Effect.Uncurried.mkEffectFn2 \state vdom -> do
     let
       { build } = state
     case vdom of
-      Halogen.VDom.Grafted g → do
+      Halogen.VDom.Grafted g -> do
         Effect.Uncurried.runEffectFn2 patchText state (Halogen.VDom.runGraft g)
       Halogen.VDom.Text _ -> do
         pure $ Halogen.VDom.mkStep $ Halogen.VDom.Step unit state patchText haltText
-      _ → do
+      _ -> do
         Effect.Uncurried.runEffectFn1 haltText state
         Effect.Uncurried.runEffectFn1 build vdom
 
 -- | Adapted from `Halogen.VDom.DOM.haltText`
-haltText ∷ forall a w. Effect.Uncurried.EffectFn1 (TextState a w) Unit
+haltText :: forall a w. Effect.Uncurried.EffectFn1 (TextState a w) Unit
 haltText =
-  Effect.Uncurried.mkEffectFn1 \_ → do
+  Effect.Uncurried.mkEffectFn1 \_ -> do
     pure unit
 
 -- | Adapted from `Halogen.VDom.DOM.ElemState`
@@ -182,12 +182,12 @@ haltText =
 -- | * removed `ns` and `name`
 -- |   * we don't need to compare and patch in DOM
 type ElemState a w =
-  { build ∷ VDomMachine a w
-  , children ∷ Array (VDomStep a w)
+  { build :: VDomMachine a w
+  , children :: Array (VDomStep a w)
   }
 
 -- | Adapted from `Halogen.VDom.DOM.buildElem`
-buildElem ∷
+buildElem ::
   forall a w.
   Effect.Uncurried.EffectFn1
     { build :: VDomMachine a w
@@ -195,12 +195,12 @@ buildElem ∷
     }
     (VDomStep a w)
 buildElem =
-  Effect.Uncurried.mkEffectFn1 \({ build, children: ch1 }) → do
+  Effect.Uncurried.mkEffectFn1 \({ build, children: ch1 }) -> do
     let
       onChild =
-        Effect.Uncurried.mkEffectFn2 \_ix child → do
+        Effect.Uncurried.mkEffectFn2 \_ix child -> do
           Effect.Uncurried.runEffectFn1 build child
-    children ← Effect.Uncurried.runEffectFn2 Halogen.VDom.Util.forE ch1 onChild
+    children <- Effect.Uncurried.runEffectFn2 Halogen.VDom.Util.forE ch1 onChild
     let
       state :: ElemState a w
       state =
@@ -210,22 +210,22 @@ buildElem =
     pure $ Halogen.VDom.mkStep $ Halogen.VDom.Step unit state patchElem haltElem
 
 -- | Adapted from `Halogen.VDom.DOM.patchElem`
-patchElem ∷
+patchElem ::
   forall a w.
   Effect.Uncurried.EffectFn2
     (ElemState a w)
     (VHTML a w)
     (VDomStep a w)
 patchElem =
-  Effect.Uncurried.mkEffectFn2 \state vdom → do
+  Effect.Uncurried.mkEffectFn2 \state vdom -> do
     let
       { build, children: ch1 } = state
     case vdom of
-      Halogen.VDom.Grafted g → do
+      Halogen.VDom.Grafted g -> do
         Effect.Uncurried.runEffectFn2 patchElem state (Halogen.VDom.runGraft g)
       Halogen.VDom.Elem _ns2 _name2 _as2 ch2 -> do
         case Data.Array.length ch1, Data.Array.length ch2 of
-          0, 0 → do
+          0, 0 -> do
             let
               nextState :: ElemState a w
               nextState =
@@ -233,34 +233,34 @@ patchElem =
                 , children: ch1
                 }
             pure $ Halogen.VDom.mkStep $ Halogen.VDom.Step unit nextState patchElem haltElem
-          _, _ → do
+          _, _ -> do
             let
               onThese =
-                Effect.Uncurried.mkEffectFn3 \_ix s v → do
+                Effect.Uncurried.mkEffectFn3 \_ix s v -> do
                   Effect.Uncurried.runEffectFn2 Halogen.VDom.step s v
 
               onThis =
-                Effect.Uncurried.mkEffectFn2 \_ s → do
+                Effect.Uncurried.mkEffectFn2 \_ s -> do
                   Effect.Uncurried.runEffectFn1 Halogen.VDom.halt s
 
               onThat =
-                Effect.Uncurried.mkEffectFn2 \_ix v → do
+                Effect.Uncurried.mkEffectFn2 \_ix v -> do
                   Effect.Uncurried.runEffectFn1 build v
-            children2 ← Effect.Uncurried.runEffectFn5 Halogen.VDom.Util.diffWithIxE ch1 ch2 onThese onThis onThat
+            children2 <- Effect.Uncurried.runEffectFn5 Halogen.VDom.Util.diffWithIxE ch1 ch2 onThese onThis onThat
             let
               nextState =
                 { build
                 , children: children2
                 }
             pure $ Halogen.VDom.mkStep $ Halogen.VDom.Step unit nextState patchElem haltElem
-      _ → do
+      _ -> do
         Effect.Uncurried.runEffectFn1 haltElem state
         Effect.Uncurried.runEffectFn1 build vdom
 
 -- | Adapted from `Halogen.VDom.DOM.haltElem`
-haltElem ∷ forall a w. Effect.Uncurried.EffectFn1 (ElemState a w) Unit
+haltElem :: forall a w. Effect.Uncurried.EffectFn1 (ElemState a w) Unit
 haltElem =
-  Effect.Uncurried.mkEffectFn1 \({ children }) → do
+  Effect.Uncurried.mkEffectFn1 \({ children }) -> do
     Effect.Uncurried.runEffectFn2 Halogen.VDom.Util.forEachE children
       Halogen.VDom.halt
 
@@ -271,13 +271,13 @@ haltElem =
 -- | * removed `ns` and `name`
 -- |   * we don't need to compare and patch in DOM
 type KeyedState a w =
-  { build ∷ VDomMachine a w
-  , children ∷ Foreign.Object.Object (VDomStep a w)
-  , length ∷ Int
+  { build :: VDomMachine a w
+  , children :: Foreign.Object.Object (VDomStep a w)
+  , length :: Int
   }
 
 -- | Adapted from `Halogen.VDom.DOM.buildKeyed`
-buildKeyed ∷
+buildKeyed ::
   forall a w.
   Effect.Uncurried.EffectFn1
     { build :: VDomMachine a w
@@ -285,12 +285,12 @@ buildKeyed ∷
     }
     (VDomStep a w)
 buildKeyed =
-  Effect.Uncurried.mkEffectFn1 \({ build, children: ch1 }) → do
+  Effect.Uncurried.mkEffectFn1 \({ build, children: ch1 }) -> do
     let
       onChild =
-        Effect.Uncurried.mkEffectFn3 \_ _ix (Tuple _ vdom) → do
+        Effect.Uncurried.mkEffectFn3 \_ _ix (Tuple _ vdom) -> do
           Effect.Uncurried.runEffectFn1 build vdom
-    children ← Effect.Uncurried.runEffectFn3 Halogen.VDom.Util.strMapWithIxE ch1 Data.Tuple.fst onChild
+    children <- Effect.Uncurried.runEffectFn3 Halogen.VDom.Util.strMapWithIxE ch1 Data.Tuple.fst onChild
     let
       state =
         { build
@@ -300,21 +300,21 @@ buildKeyed =
     pure $ Halogen.VDom.mkStep $ Halogen.VDom.Step unit state patchKeyed haltKeyed
 
 -- | Adapted from `Halogen.VDom.DOM.patchKeyed`
-patchKeyed ∷
+patchKeyed ::
   forall a w.
   Effect.Uncurried.EffectFn2
     (KeyedState a w)
     (VHTML a w)
     (VDomStep a w)
 patchKeyed =
-  Effect.Uncurried.mkEffectFn2 \state vdom → do
+  Effect.Uncurried.mkEffectFn2 \state vdom -> do
     let
       { build, children: ch1, length: len1 } = state
     case vdom of
-      Halogen.VDom.Grafted g → do
+      Halogen.VDom.Grafted g -> do
         Effect.Uncurried.runEffectFn2 patchKeyed state (Halogen.VDom.runGraft g)
-      Halogen.VDom.Keyed _ns2 _name2 _as2 ch2 → case len1, Data.Array.length ch2 of
-        0, 0 → do
+      Halogen.VDom.Keyed _ns2 _name2 _as2 ch2 -> case len1, Data.Array.length ch2 of
+        0, 0 -> do
           let
             nextState =
               { build
@@ -322,20 +322,20 @@ patchKeyed =
               , length: 0
               }
           pure $ Halogen.VDom.mkStep $ Halogen.VDom.Step unit nextState patchKeyed haltKeyed
-        _, len2 → do
+        _, len2 -> do
           let
             onThese =
-              Effect.Uncurried.mkEffectFn4 \_ _ix' s (Tuple _ v) → do
+              Effect.Uncurried.mkEffectFn4 \_ _ix' s (Tuple _ v) -> do
                 Effect.Uncurried.runEffectFn2 Halogen.VDom.step s v
 
             onThis =
-              Effect.Uncurried.mkEffectFn2 \_ s → do
+              Effect.Uncurried.mkEffectFn2 \_ s -> do
                 Effect.Uncurried.runEffectFn1 Halogen.VDom.halt s
 
             onThat =
-              Effect.Uncurried.mkEffectFn3 \_ _ix (Tuple _ v) → do
+              Effect.Uncurried.mkEffectFn3 \_ _ix (Tuple _ v) -> do
                 Effect.Uncurried.runEffectFn1 build v
-          children2 ← Effect.Uncurried.runEffectFn6 Halogen.VDom.Util.diffWithKeyAndIxE ch1 ch2 Data.Tuple.fst onThese onThis onThat
+          children2 <- Effect.Uncurried.runEffectFn6 Halogen.VDom.Util.diffWithKeyAndIxE ch1 ch2 Data.Tuple.fst onThese onThis onThat
           let
             nextState =
               { build
@@ -343,23 +343,23 @@ patchKeyed =
               , length: len2
               }
           pure $ Halogen.VDom.mkStep $ Halogen.VDom.Step unit nextState patchKeyed haltKeyed
-      _ → do
+      _ -> do
         Effect.Uncurried.runEffectFn1 haltKeyed state
         Effect.Uncurried.runEffectFn1 build vdom
 
 -- | Adapted from `Halogen.VDom.DOM.haltKeyed`
-haltKeyed ∷ ∀ a w. Effect.Uncurried.EffectFn1 (KeyedState a w) Unit
+haltKeyed :: forall a w. Effect.Uncurried.EffectFn1 (KeyedState a w) Unit
 haltKeyed =
-  Effect.Uncurried.mkEffectFn1 \({ children }) → do
+  Effect.Uncurried.mkEffectFn1 \({ children }) -> do
     Effect.Uncurried.runEffectFn2 Halogen.VDom.Util.forInE children do
-      Effect.Uncurried.mkEffectFn2 \_ s → do
+      Effect.Uncurried.mkEffectFn2 \_ s -> do
         Effect.Uncurried.runEffectFn1 Halogen.VDom.halt s
 
 -- | Combined `Halogen.VDom.Driver.WidgetState` and `Halogen.VDom.DOM.WidgetState`
 type WidgetState action slots =
-  { build ∷ VDomMachine action slots
+  { build :: VDomMachine action slots
   , renderChildRef :: Effect.Ref.Ref (ChildRenderer action slots)
-  , widget ∷ Maybe (Halogen.VDom.Step (HTMLThunk action slots) Unit)
+  , widget :: Maybe (Halogen.VDom.Step (HTMLThunk action slots) Unit)
   }
 
 -- | Copied from Halogen.VDom.Driver which is not exported
@@ -369,7 +369,7 @@ type HTMLThunk action slots =
     action
 
 -- | Adapted from `Halogen.VDom.Driver.mkSpec.buildWidget.render`
-buildWidget ∷
+buildWidget ::
   forall action slots.
   Effect.Uncurried.EffectFn1
     { build :: VDomMachine action slots
@@ -378,7 +378,7 @@ buildWidget ∷
     }
     (VDomStep action slots)
 buildWidget =
-  Effect.Uncurried.mkEffectFn1 \({ build, renderChildRef, slot }) → case slot of
+  Effect.Uncurried.mkEffectFn1 \({ build, renderChildRef, slot }) -> case slot of
     Halogen.Component.ComponentSlot cs -> do
       Effect.Uncurried.runEffectFn2 renderComponentSlot { build, renderChildRef, widget: Nothing } cs
     Halogen.Component.ThunkSlot t -> do
@@ -386,15 +386,15 @@ buildWidget =
       pure $ Halogen.VDom.mkStep $ Halogen.VDom.Step (Halogen.VDom.extract step) { build, renderChildRef, widget: Just step } patchWidget haltWidget
 
 -- | Adapted from `Halogen.VDom.Driver.mkSpec.buildWidget.patch`
-patchWidget ∷
+patchWidget ::
   forall a w.
   Effect.Uncurried.EffectFn2
     (WidgetState a w)
     (VHTML a w)
     (VDomStep a w)
 patchWidget =
-  Effect.Uncurried.mkEffectFn2 \state vdom → case vdom of
-    Halogen.VDom.Grafted g → do
+  Effect.Uncurried.mkEffectFn2 \state vdom -> case vdom of
+    Halogen.VDom.Grafted g -> do
       Effect.Uncurried.runEffectFn2 patchWidget state (Halogen.VDom.runGraft g)
     Halogen.VDom.Widget slot
       | Just step <- state.widget -> case slot of
@@ -422,7 +422,7 @@ renderComponentSlot =
     pure $ Halogen.VDom.mkStep $ Halogen.VDom.Step unit (state { widget = Nothing }) patchWidget haltWidget
 
 -- | Adapted from `Halogen.VDom.Driver.mkSpec.done`
-haltWidget ∷ forall a w. Effect.Uncurried.EffectFn1 (WidgetState a w) Unit
+haltWidget :: forall a w. Effect.Uncurried.EffectFn1 (WidgetState a w) Unit
 haltWidget =
   Effect.Uncurried.mkEffectFn1 \st -> case st.widget of
     Just step -> Effect.Uncurried.runEffectFn1 Halogen.VDom.halt step
@@ -431,27 +431,27 @@ haltWidget =
 -- | Adapted from `Halogen.VDom.DOM.ThunkState`
 -- | * set machine output to `Unit` in place of `Web.DOM.Node.Node`
 type ThunkState (f :: Type -> Type) i a w =
-  { thunk ∷ Halogen.VDom.Thunk.Thunk f i
-  , vdom ∷ Halogen.VDom.Step (VHTML a w) Unit
+  { thunk :: Halogen.VDom.Thunk.Thunk f i
+  , vdom :: Halogen.VDom.Step (VHTML a w) Unit
   }
 
 -- | Adapted from `Halogen.VDom.Thunk.buildThunk`
-buildThunk ∷
+buildThunk ::
   forall f i a w.
   VDomMachine a w ->
-  (f i → VHTML a w) →
+  (f i -> VHTML a w) ->
   Halogen.VDom.Machine (Halogen.VDom.Thunk.Thunk f i) Unit
 buildThunk build toVDom = renderThunk
   where
-  renderThunk ∷ Halogen.VDom.Machine (Halogen.VDom.Thunk.Thunk f i) Unit
+  renderThunk :: Halogen.VDom.Machine (Halogen.VDom.Thunk.Thunk f i) Unit
   renderThunk =
-    Effect.Uncurried.mkEffectFn1 \t → do
-      vdom ← Effect.Uncurried.runEffectFn1 build (toVDom (Halogen.VDom.Thunk.runThunk t))
+    Effect.Uncurried.mkEffectFn1 \t -> do
+      vdom <- Effect.Uncurried.runEffectFn1 build (toVDom (Halogen.VDom.Thunk.runThunk t))
       pure $ Halogen.VDom.mkStep $ Halogen.VDom.Step (Halogen.VDom.extract vdom) { thunk: t, vdom } patchThunk haltThunk
 
-  patchThunk ∷ Effect.Uncurried.EffectFn2 (ThunkState f i a w) (Halogen.VDom.Thunk.Thunk f i) (Halogen.VDom.Step (Halogen.VDom.Thunk.Thunk f i) Unit)
+  patchThunk :: Effect.Uncurried.EffectFn2 (ThunkState f i a w) (Halogen.VDom.Thunk.Thunk f i) (Halogen.VDom.Step (Halogen.VDom.Thunk.Thunk f i) Unit)
   patchThunk =
-    Effect.Uncurried.mkEffectFn2 \state t2 → do
+    Effect.Uncurried.mkEffectFn2 \state t2 -> do
       let
         { vdom: prev } = state
       -- NOTE `unsafeEqThunk` along with many types are not exported
@@ -460,10 +460,10 @@ buildThunk build toVDom = renderThunk
       -- if Fn.runFn2 unsafeEqThunk t1 t2
       --   then pure $ M.mkStep $ M.Step (M.extract prev) state patchThunk haltThunk
       --   else do
-      vdom ← Effect.Uncurried.runEffectFn2 Halogen.VDom.step prev (toVDom (Halogen.VDom.Thunk.runThunk t2))
+      vdom <- Effect.Uncurried.runEffectFn2 Halogen.VDom.step prev (toVDom (Halogen.VDom.Thunk.runThunk t2))
       pure $ Halogen.VDom.mkStep $ Halogen.VDom.Step (Halogen.VDom.extract vdom) { vdom, thunk: t2 } patchThunk haltThunk
 
-  haltThunk ∷ Effect.Uncurried.EffectFn1 (ThunkState f i a w) Unit
+  haltThunk :: Effect.Uncurried.EffectFn1 (ThunkState f i a w) Unit
   haltThunk =
-    Effect.Uncurried.mkEffectFn1 \state → do
+    Effect.Uncurried.mkEffectFn1 \state -> do
       Effect.Uncurried.runEffectFn1 Halogen.VDom.halt state.vdom
