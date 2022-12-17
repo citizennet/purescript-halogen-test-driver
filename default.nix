6d3fd96a0b59d47bf99a1519517aad1802aa074c
Mason Mackaman <masondeanm@aol.com>
2022-12-17 01:46:47 -0500

    Switch dependencies to strings

--- default.nix
+++ default.nix
@@ -2,10 +2,10 @@
   perSystem = { config, self', inputs', system, pkgs, ... }:
 
     let
-      dependencies = with config.purs-nix.ps-pkgs; [
-        halogen
-        halogen-vdom
-        pre
+      dependencies = [
+        "halogen"
+        "halogen-vdom"
+        "pre"
       ];
 
       ps = config.purs-nix.purs {
